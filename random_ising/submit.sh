49e6d49a6b09b0881537c30060a978caea88c503
Rexirium <rexirium@qq.com>
2026-09-15 00:01:49 +0800

    modify some config and shell

--- random_ising/submit.sh
+++ random_ising/submit.sh
@@ -2,8 +2,8 @@
 #SBATCH -A hpc1906185151
 #SBATCH --partition=C064M1024G
 #SBATCH --qos=low
-#SBATCH -J wm2-job-20260914-zephyr
-#SBATCH --nodes=4
+#SBATCH -J wm2-job-20260915-zephyr
+#SBATCH --nodes=6
 #SBATCH --cpus-per-task=8
 #SBATCH --ntasks-per-node=7
 #SBATCH --time=7200
@@ -18,4 +18,4 @@ module load julia
 export OPENBLAS_NUM_THREADS=1
 export OMP_NUM_THREADS=1
 # 不要再加 srun：SlurmClusterManager 会自行启动 worker。
-julia --startup-file=no --threads=2 random_ising/run_slurm.jl
+julia --startup-file=no --threads=4 random_ising/run_slurm.jl
